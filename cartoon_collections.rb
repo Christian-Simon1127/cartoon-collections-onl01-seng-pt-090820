67c33de4e24d6f20ca6ac9099e29c093d1987268
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-19 22:36:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,26 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index() { |item, index|
+    puts "#{index+1}. #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.collect {|item| item.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  counter = 0
+  
+  while counter < 
+  if array.find{|item| item.length < 3} == nil 
+    false
+  else 
+    true 
+  end
+  end
 end
 
 def find_the_cheese# code an argument here
