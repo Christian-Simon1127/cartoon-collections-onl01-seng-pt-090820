738f9b5083ade6a59963753db2a14cee52a4624a
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-19 22:43:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)# code an argument here
   while counter < array.length
     if array[counter].length < 4
       value = true
-      break
+      counter += 1
     end
   end
 end
